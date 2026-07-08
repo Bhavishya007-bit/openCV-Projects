ab1829637cf8f2e08124d1d6683f4873b050d3c8
Bhavishya Gaur <bhavishyagaur6@gmail.com>
2026-07-08 20:13:03 +0530

    Gesture volume control begins

--- Gesture Volume Control/main.py
+++ Gesture Volume Control/main.py
@@ -1,7 +1,7 @@
 import cv2
 import time
 import numpy as np
-
+import HandTrackingModule as htm
 
 wCam, hCam = 1280, 720
 
@@ -10,8 +10,21 @@ cap.set(3, wCam)
 cap.set(4, hCam)
 pTime = 0
 
+detector = htm.handDetector(detectionCon=0.7)
+
+
 while True:
     success, img = cap.read()
+    img = detector.findHands(img)
+    lmlist = detector.findPosition(img, draw=False)
+    if len(lmlist) != 0:
+        print(lmlist[4], lmlist[8])
+
+        x1, y1 = lmlist[4][1], lmlist[4][2]
+        x2, y2 = lmlist[8][1], lmlist[8][2]
+        cv2.circle(img, (x1,y1), 15, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, (x2,y2), 15, (255, 0, 255), cv2.FILLED)
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
     cTime = time.time()
     fps = 1/(cTime- pTime)
