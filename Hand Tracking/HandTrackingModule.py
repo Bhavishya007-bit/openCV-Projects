8c56ffc8745a2e8482e27f21da18c8079ca11513
Bhavishya Gaur <bhavishyagaur6@gmail.com>
2026-07-08 19:48:26 +0530

    Gesture volume control begins

--- Hand Tracking/HandTrackingModule.py
+++ Hand Tracking/HandTrackingModule.py
@@ -21,7 +21,6 @@ class handDetector():
         if self.results.multi_hand_landmarks:
             for handLms in self.results.multi_hand_landmarks:
                 if draw:
-
                     self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
         return img
     
